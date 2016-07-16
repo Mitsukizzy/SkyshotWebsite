2e0ec741610144feb73fc689e821838791573987
Aaron Hong <hong.aaron.c@gmail.com>
2016-07-15 21:45:54 -0700

    Attempt to rescale FB plugin

--- news.js
+++
@@ -1,7 +0,0 @@
-function appendCSS() {
-var cssLink = document.createElement("link")
-    cssLink.href = "news.css";
-    cssLink.rel = "stylesheet";
-    cssLink.type = "text/css";
-    frames[0].document.body.appendChild(cssLink);
-}
