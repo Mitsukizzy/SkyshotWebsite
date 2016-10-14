a93c58c2b81ddcd1f733d979982958a66dfd26b6
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-13 18:03:56 -0700

    Updated landing page art and reimplemented parallax to be less laggy

--- landing.js
+++ landing.js
@@ -1,7 +1,20 @@
+var update = false;
+
+function startParallax() {
+    setInterval(unlockParallax, 8);
+}
+
+function unlockParallax() {
+    update = true;
+}
+
 function parallax() {
-    var layers = document.getElementsByClassName("splashart-layer");
-    for (var i = 0; i < layers.length; i++) {
-        var parallaxOffset = -document.body.scrollTop / layers[i].dataset.speed;
-        layers[i].style.top = parallaxOffset + "px";
+    if (update) {
+        var layers = document.getElementsByClassName("splashart-layer");
+        for (var i = 0; i < layers.length; i++) {
+            var parallaxOffset = document.body.scrollTop / layers[i].dataset.speed;
+            layers[i].style.transform = "translateY(" + parallaxOffset + "px)";
+        }
+        update = false;
     }
 }
