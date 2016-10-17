be715d4d6e181f5e7528addec09c304202106aaf
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-17 06:21:41 -0700

    Rounded edges on images on landing page. Reinserted frame lock code for parallax as comment in case needed.

--- landing.js
+++ landing.js
@@ -1,6 +1,14 @@
 var update = true;
 var layers = document.getElementsByClassName("splashart-layer");
 
+/*function startParallax() {
+    setInterval(unlockParallax, 8);
+}
+
+function unlockParallax() {
+    update = true;
+}*/
+
 function parallax() {
     //if (update) {
         for (var i = 0; i < layers.length; i++) {
