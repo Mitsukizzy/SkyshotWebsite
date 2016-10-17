28c33e1f5c4db8a52f512e65fc344a8637059145
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-17 06:13:11 -0700

    Made parallax way faster

--- landing.js
+++ landing.js
@@ -1,20 +1,12 @@
 var update = true;
-
-function startParallax() {
-    setInterval(unlockParallax, 8);
-}
-
-function unlockParallax() {
-    update = true;
-}
+var layers = document.getElementsByClassName("splashart-layer");
 
 function parallax() {
-    if (update) {
-        var layers = document.getElementsByClassName("splashart-layer");
+    //if (update) {
         for (var i = 0; i < layers.length; i++) {
             var parallaxOffset = document.body.scrollTop / layers[i].dataset.speed;
             layers[i].style.transform = "translateY(" + parallaxOffset + "px)";
         }
         update = false;
-    }
+    //}
 }
