e8401e5eaadc619c0f3d8f595e3d3ac44ffbe6f1
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-17 21:28:18 -0700

    Centered navbar refinement. Removed interpolation on parallax since it is already efficient now.

--- landing.js
+++ landing.js
@@ -1,7 +1,8 @@
 var update = true;
 var layers = document.getElementsByClassName("splashart-layer");
 
-var navbar = document.getElementById("navbar_main");
+var navbar = document.getElementById("navbar-main");
+var logo = document.getElementById("navbar-logo");
 
 /*function startParallax() {
     setInterval(unlockParallax, 8);
@@ -22,17 +23,30 @@ function parallax() {
 }
 
 function fixNavbar() {
-    if (!navbar) {
-        navbar = document.getElementById("navbar_main");
-    }
+    if (!navbar)
+        navbar = document.getElementById("navbar-main");
+    if (!logo)
+        logo = document.getElementById("navbar-logo");
+
     if (document.body.scrollTop > document.body.clientHeight) {
         navbar.style.position = "fixed";
         navbar.style.top = "0px";
         navbar.style.backgroundColor = "#222";
+        document.getElementById("navbar-logo");
+        logo.style.opacity = "1";
+        logo.style.minWidth = "150px";
+        logo.style.width = "auto";
+        logo.style.marginLeft = "50px";
+        logo.style.marginRight = "50px";
     }
     else {
         navbar.style.position = "absolute";
         navbar.style.top = "100vh";
         navbar.style.backgroundColor = "#000";
+        logo.style.opacity = "0";
+        logo.style.minWidth = "0";
+        logo.style.width = "0";
+        logo.style.marginLeft = "0";
+        logo.style.marginRight = "0";
     }
 }
