dd1174b36f1ca943ea780b5fdf814e62947acec9
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-17 21:00:08 -0700

    Centered navbar test

--- landing.js
+++ landing.js
@@ -1,6 +1,8 @@
 var update = true;
 var layers = document.getElementsByClassName("splashart-layer");
 
+var navbar = document.getElementById("navbar_main");
+
 /*function startParallax() {
     setInterval(unlockParallax, 8);
 }
@@ -17,4 +19,20 @@ function parallax() {
         }
         update = false;
     //}
+}
+
+function fixNavbar() {
+    if (!navbar) {
+        navbar = document.getElementById("navbar_main");
+    }
+    if (document.body.scrollTop > document.body.clientHeight) {
+        navbar.style.position = "fixed";
+        navbar.style.top = "0px";
+        navbar.style.backgroundColor = "#222";
+    }
+    else {
+        navbar.style.position = "absolute";
+        navbar.style.top = "100vh";
+        navbar.style.backgroundColor = "#000";
+    }
 }
