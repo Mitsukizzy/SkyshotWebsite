eae3e60d1ec4630bcff0d0b6b1bb6497bfdcf6d1
Aaron Hong <hong.aaron.c@gmail.com>
2016-10-19 20:46:05 -0700

    Disabling parallax on mobile attempt #3

--- landing.js
+++ landing.js
@@ -14,8 +14,29 @@ function unlockParallax() {
     update = true;
 }*/
 
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 function parallax() {
-    if (window.innerWidth <= 800 && window.innerHeight <= 600)
+    if (window.innerWidth <= 800 && window.innerHeight <= 600 || isMobile.any())
         return;
     //if (update) {
         for (var i = 0; i < layers.length; i++) {
